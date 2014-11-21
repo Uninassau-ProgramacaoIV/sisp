d715eeeb6d0cff0ddea7697c1a3d95acfdb4682c
Romario <romario.melos@gmail.com>
2014-11-20 21:38:26 -0300

    Signed-off-by: Romario <romario.melos@gmail.com>

---
+++ src/java/mvc/cargo/Cargo.java
@@ -0,0 +1,35 @@
+
+package mvc.cargo;
+
+public class Cargo {
+    private int codigo;
+    private String descricao;
+    
+    public Cargo() {
+        
+    }
+    
+    public Cargo(int codigo, String descricao) {
+        this.codigo = codigo;
+        this.descricao = descricao;
+    }
+
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    
+    
+}
---
+++ src/java/mvc/funcionario/Funcionario.java
@@ -0,0 +1,43 @@
+package mvc.funcionario;
+
+import mvc.cargo.Cargo;
+
+public class Funcionario {
+    
+    private int codigo;
+    private String nome;
+    private Cargo cargo;
+    private String nascimento;
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Cargo getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(Cargo cargo) {
+        this.cargo = cargo;
+    }
+
+    public String getNascimento() {
+        return nascimento;
+    }
+
+    public void setNascimento(String nascimento) {
+        this.nascimento = nascimento;
+    }
+}
