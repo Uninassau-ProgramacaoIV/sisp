7dd98bac429e60aab50346c4b99a7ba64e0d03b7
Romario <romario.melos@gmail.com>
2014-11-20 23:38:16 -0300

    Finalizando primeiro dia de trabalho. ;)

--- src/java/mvc/funcionario/model/RamFuncionarioDAO.java
+++ src/java/mvc/funcionario/model/RamFuncionarioDAO.java
@@ -1,18 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 package mvc.funcionario.model;
 
 import java.util.List;
 import mvc.funcionario.Funcionario;
 
-/**
- *
- * @author notebook
- */
 public class RamFuncionarioDAO implements FuncionarioDAO {
 
     @Override
